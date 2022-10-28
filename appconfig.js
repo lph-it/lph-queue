33fad3ff1725ee79306f5743ba3207f7399b6760
tharathip.lph <tharathip.lph@gmail.com>
2022-10-28 09:10:41 +0700

    deploy lph-queue

---
+++ appconfig.js
@@ -0,0 +1,6 @@
+const appconfig = {
+    serverUrl: 'http://localhost:4004',
+    //serverUrl: 'http://172.16.0.29:4004',
+    //serverUrl: 'http://172.16.188.11:4004',
+};
+window['appconfig'] = appconfig;
