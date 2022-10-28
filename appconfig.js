705c0e2298f691a2cfd23551f2af019f0744e3ad
tharathip.lph <tharathip.lph@gmail.com>
2022-10-28 09:22:36 +0700

    update appconfig

--- appconfig.js
+++ appconfig.js
@@ -1,6 +1,4 @@
 const appconfig = {
-    serverUrl: 'http://localhost:4004',
-    //serverUrl: 'http://172.16.0.29:4004',
-    //serverUrl: 'http://172.16.188.11:4004',
+    serverUrl: 'http://linebot.lampanghospital.com:4004',
 };
 window['appconfig'] = appconfig;
